56e84b26730f407945aef5951e50875068e5b2e0
Nikita Kobylin <imayfon@gmail.com>
2018-04-07 14:13:05 +0500

    Create minigame controller

---
+++ Assets/Hackathon2018.1/Scripts/MinigameController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MinigameController : MonoBehaviour {
+
+    public static MinigameController instance;
+
+    private const string globeSceneName = "Main";
+
+	void Awale ()
+    {
+        instance = this;
+    }
+
+    public void GameOver()
+    {
+        SceneManager.LoadScene(globeSceneName);
+    }
+
+    public void GameComplete()
+    {
+        SceneManager.LoadScene(globeSceneName);
+    }
+
+}
