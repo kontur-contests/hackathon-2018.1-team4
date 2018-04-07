8dda00d8d5be28826f6638651e61afc3e5aa29e5
vkasatkin <vkasatkin@skbkontur.ru>
2018-04-07 19:38:35 +0500

    ShitClean fix

--- Assets/Hackathon2018.1/Scripts/MG-ShitClean/BoxScript.cs
+++ Assets/Hackathon2018.1/Scripts/MG-ShitClean/BoxScript.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 using UnityEngine.UI;
@@ -9,29 +8,48 @@ public class BoxScript : MonoBehaviour {
     protected List<GameObject> pieces = new List<GameObject>();
     private GameObject LeftUpPoint;
     private GameObject RightDownPoint;
-    private Text CountText;
+    private Text TimeText;
+    private Text ShitCount;
+
+    private float time = 30;
+    private int CountToWin = 5;
+    private int Count = 100;
 
     public void Start()
     {
         LeftUpPoint = GameObject.Find("LeftUpPoint");
         RightDownPoint = GameObject.Find("RightDownPoint");
-        CountText = GameObject.Find("CountText").GetComponent<Text>();
-        
+        TimeText = GameObject.Find("TimeCount").GetComponent<Text>();
+        ShitCount = GameObject.Find("ShitCount").GetComponent<Text>();
+
         pieces.AddRange(GameObject.FindGameObjectsWithTag("ShitPiece"));
     }
 
     public void Update()
     {
+        if (time <= 0)
+        {
+            MinigameController.instance.GameOver();
+        }
+
+        if (Count <= CountToWin)
+        {
+            MinigameController.instance.GameComplete();
+        }
+
+        time -= Time.deltaTime;
+
+        TimeText.text = time.ToString();
+        ShitCount.text = (Count - CountToWin).ToString();
     }
 
     private void OnTriggerStay2D(Collider2D e)
     {
-        var count = pieces.Count(piece => 
+        Count = pieces.Count(piece => 
             piece.transform.position.x >= LeftUpPoint.transform.position.x &&
             piece.transform.position.x <= RightDownPoint.transform.position.x &&
             piece.transform.position.y <= LeftUpPoint.transform.position.y &&
             piece.transform.position.y >= RightDownPoint.transform.position.y
         );
-        CountText.text = count.ToString();
     }
 }
