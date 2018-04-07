08888d1a394c49e5672bb999f5ee9a7a8eb6583f
vkasatkin <vkasatkin@skbkontur.ru>
2018-04-07 18:44:39 +0500

    Прототип ShitClean

---
+++ Assets/Hackathon2018.1/Scripts/MG-ShitClean/BoxScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.UI;
+
+public class BoxScript : MonoBehaviour {
+
+    protected List<GameObject> pieces = new List<GameObject>();
+    private GameObject LeftUpPoint;
+    private GameObject RightDownPoint;
+    private Text CountText;
+
+    public void Start()
+    {
+        LeftUpPoint = GameObject.Find("LeftUpPoint");
+        RightDownPoint = GameObject.Find("RightDownPoint");
+        CountText = GameObject.Find("CountText").GetComponent<Text>();
+        
+        pieces.AddRange(GameObject.FindGameObjectsWithTag("ShitPiece"));
+    }
+
+    public void Update()
+    {
+    }
+
+    private void OnTriggerStay2D(Collider2D e)
+    {
+        var count = pieces.Count(piece => 
+            piece.transform.position.x >= LeftUpPoint.transform.position.x &&
+            piece.transform.position.x <= RightDownPoint.transform.position.x &&
+            piece.transform.position.y <= LeftUpPoint.transform.position.y &&
+            piece.transform.position.y >= RightDownPoint.transform.position.y
+        );
+        CountText.text = count.ToString();
+    }
+}
---
+++ Assets/Hackathon2018.1/Scripts/MG-ShitClean/ShovelScript.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShovelScript : MonoBehaviour {
+
+    GameObject shovel;
+    HingeJoint2D joint;
+    Rigidbody2D rigidbody;
+    bool rotation = false;
+
+    float moveSpeed = 0.1f;
+
+    // Use this for initialization
+    void Start () {
+        shovel = GameObject.Find("Shovel");
+        joint = shovel.GetComponent<HingeJoint2D>();
+        rigidbody = shovel.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RotationLogic();
+        MoveLogic();
+    }
+
+    private void MoveLogic()
+    {
+        var x = shovel.transform.position.x;
+        var y = shovel.transform.position.y;
+        var z = shovel.transform.position.z;
+
+        if (Input.GetKey(KeyCode.W)){
+            y += moveSpeed;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            x -= moveSpeed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            y -= moveSpeed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            x += moveSpeed;
+        }
+
+        shovel.transform.position = new Vector3(x, y, z);
+    }
+
+    private void RotationLogic()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            joint.useMotor = true;
+            rotation = true;
+        }
+
+        var moduleRotation = Mathf.Abs(shovel.transform.localRotation.z);
+
+        if (moduleRotation >= 0.8)
+            rotation = false;
+
+        if (!rotation && moduleRotation <= 0.05 && moduleRotation >= 0)
+        {
+            joint.useMotor = false;
+            rigidbody.angularVelocity = 0;
+        }
+    }
+}
