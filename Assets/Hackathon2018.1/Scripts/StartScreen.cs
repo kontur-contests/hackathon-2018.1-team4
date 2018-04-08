8a2b8c9c7adf67f3c409a994f1e160107ea5a466
Nikita Kobylin <imayfon@gmail.com>
2018-04-08 09:01:22 +0500

    Add intro

---
+++ Assets/Hackathon2018.1/Scripts/StartScreen.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartScreen : MonoBehaviour {
+
+    public GameObject StartScreenPrefab;
+
+    public static StartScreen instance;
+
+    void Start () {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this);
+            Instantiate(StartScreenPrefab);
+        }
+	}
+
+}
