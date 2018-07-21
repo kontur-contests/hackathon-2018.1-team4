19185050dad1bbeaea7f9f0b25becdb3f5a82db8
Nikita Kobylin <imayfon@gmail.com>
2018-07-22 01:34:34 +0500

    Added localization

--- Assets/Hackathon2018.1/Scripts/EndGameScirpt.cs
+++ Assets/Hackathon2018.1/Scripts/EndGameScirpt.cs
@@ -3,26 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class EndGameScirpt : MonoBehaviour {
+public class EndGameScirpt : MonoBehaviour
+{
 
     public Text text;
 
     public int scoreTowin = 150;
 
-	void Start () {
-		if(ScoreManager.instance.score >= scoreTowin)
+    void Start()
+    {
+        if (ScoreManager.instance.score >= scoreTowin)
         {
-            text.text = "Баланс сил в мире восстановлен и народы коров и людей снова могут жить в мире.  Победа за вами!";
+            string t1 = Application.systemLanguage == SystemLanguage.Russian ? "Баланс сил в мире восстановлен и народы коров и людей снова могут жить в мире.  Победа за вами!" :
+                "The balance of forces in the world is restored and the peoples of cows and people can again live in peace. Victory is yours!";
+            text.text = t1;
         }
         else
         {
-            text.text = "Озоновые дыры стали дырами в вашей репутации. Толпа сжигает вас на костре.";
+            string t1 = Application.systemLanguage == SystemLanguage.Russian ? "Озоновые дыры стали дырами в вашей репутации. Толпа сжигает вас на костре." :
+                "Ozone holes have become holes in your reputation. The crowd burns you at the stake.";
+            text.text = t1;
         }
-	}
+    }
 
     private void Update()
     {
-        if(Input.anyKey)
+        if (Input.anyKey)
         {
             ResetGame();
         }
---
+++ Assets/Hackathon2018.1/Scripts/Localizator.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class Localizator : MonoBehaviour {
+
+    public bool debugEn;
+
+    public Font ruFont;
+    public Font enFont;
+
+    public int ruFontSize;
+    public int enFontSize = 21;
+
+    public string ruText;
+    public string enText;
+
+    private Text text;
+
+    void Update()
+    {
+        text = GetComponent<Text>();
+        if (text)
+        {
+            text.text = Application.systemLanguage == SystemLanguage.Russian && !debugEn ? ruText : enText;
+            text.font = Application.systemLanguage == SystemLanguage.Russian && !debugEn ? ruFont : enFont;
+            text.fontSize = Application.systemLanguage == SystemLanguage.Russian && !debugEn ? ruFontSize : enFontSize;
+        }
+
+    }
+	
+}
